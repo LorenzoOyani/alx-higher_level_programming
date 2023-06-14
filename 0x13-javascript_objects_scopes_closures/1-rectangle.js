c5afd427aee7f87d4586a9d3ea9ab43780d61445
Jerumeh lawrence <jerumehlawrence@gmail.com>
2023-06-14 17:45:37 +0100

    new commit

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,4 +1,12 @@
 #!/usr/bin/node
+// module.exports = class Rectangle {
+//   constructor (w, h) {
+// this.width = w;
+// this.height = h;
+//   }
+// };
+
+// OR
 class Rectangle {
   constructor (w, h) {
     this.width = w;
